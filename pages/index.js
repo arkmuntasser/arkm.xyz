92f7126845b36c1bc249267fc6c54f6873234101
Amir R Muntasser <arkmuntasser@gmail.com>
2021-03-20 02:53:25 -0700

    some basic styles

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,45 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import styles from '../styles/Home.module.css';
 import { getAllNodes } from 'next-mdx';
+import { format } from 'date-fns';
 
-export default function Home({posts}) {
+import postStyles from '../styles/Post.module.css';
+
+export default function Home({ posts }) {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>ARKM - Welcome to die!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
 				{posts.length ? (
 					<section>
 						<h2>Posts</h2>
-						{posts.map((post) => (
-							<article key={post.slug}>
-								<Link href={post.url} passHref>
-									<a>
-										<h3>{post.frontMatter.title}</h3>
-									</a>
-								</Link>
-							</article>
-						))}
+						<div className={postStyles.posts}>
+							{posts.map((post) => (
+								<article className={postStyles.post} key={post.slug}>
+									<header>
+										<Link href={post.url} passHref>
+											<a>
+												<h3>{post.frontMatter.title}</h3>
+											</a>
+										</Link>
+										<time dateTime={post.frontMatter.date}>{format(new Date(post.frontMatter.date), 'MM.dd.yyyy')}</time>
+									</header>
+									<p>{post.frontMatter.excerpt}</p>
+								</article>
+							))}
+						</div>
 					</section>
 				) : null}
-
       </main>
-    </div>
+    </>
   )
 }
 
