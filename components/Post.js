a9d4da6850a4c4347181ca8c063b4f6eeebb0949
Amir R Muntasser <arkmuntasser@gmail.com>
2021-04-07 21:07:41 -0700

    new post, stylez

--- components/Post.js
+++ components/Post.js
@@ -3,10 +3,13 @@ import { format } from 'date-fns';
 import styles from '../styles/Post.module.css';
 
 export default function Post({ data, style }) {
+	const date = new Date(data.frontMatter.date);
+	const correctedDate = date.valueOf() + date.getTimezoneOffset() * 60 * 1000;
+
   return (
 		<article className={styles.post} style={style}>
 			<header>
-				<time dateTime={data.frontMatter.date}>{format(new Date(data.frontMatter.date), 'MM.dd.yyyy')}</time>
+				<time dateTime={data.frontMatter.date}>{format(correctedDate, 'MM.dd.yyyy')}</time>
 				<Link href={data.url} passHref>
 					<a>
 						<h3 className="title">{data.frontMatter.title}</h3>
