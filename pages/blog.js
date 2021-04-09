c9c6a50fadaeb3b810798e0ae05c564cdd8d9f63
Amir R Muntasser <arkmuntasser@gmail.com>
2021-04-09 01:51:22 -0700

    content

--- pages/blog.js
+++ pages/blog.js
@@ -12,11 +12,12 @@ export default function Blog({ posts, cats }) {
 			<main>
 				<Section>
 					<h1 style={{ transitionDuration: `${400 + 80 * 1}ms` }}>The Blog</h1>
-					<p style={{ transitionDuration: `${400 + 80 * 2}ms` }}>Behold! My blog.</p>
-					<p style={{ transitionDuration: `${400 + 80 * 3}ms` }}>I typically write, when I write, about tech stuff, but I might blog about anything. Including, but not limited to social justice issues, movies, games, whatever. I'm not a professional writer; first draft, best draft.</p>
+					<p style={{ transitionDuration: `${400 + 80 * 2}ms` }}>
+						This is where you can find a collection of all my writings. Topics will range from development, to politics, to social justice issues. Whatever is on my mind, really. Have yourself a read and hopefully you get something out of it, but these posts are for me.
+					</p>
 				</Section>
 				<Section layout="small">
-					<h2 style={{ transitionDuration: `${400 + 80 * 4}ms` }}>Categories</h2>
+					<h2 style={{ transitionDuration: `${400 + 80 * 3}ms` }}>Categories</h2>
 					<Chips items={cats}/>
 				</Section>
 				<PostGroup
