dd32e29e1d2da5fb8a529a5ecb54360239b2dbe0
Amir R Muntasser <arkmuntasser@gmail.com>
2021-05-29 01:14:34 -0700

    fixed white space issue

--- buildFeed.js
+++ buildFeed.js
@@ -4,27 +4,27 @@ const { getAllNodes } = require('next-mdx');
 
 function generateRSS(posts, filter = 'all') {
 	const siteUrl = 'https://arkm.xyz';
-  const feed = new RSS({
-    title: `Amir R Muntasser's ${filter === 'all' ? 'Firehose' : filter === 'posts' ? 'Blog Posts' : 'Weaknotes'}`,
-    site_url: `${siteUrl}${filter === 'all' ? '' : filter === 'posts' ? '/blog' : '/weaknotes'}`,
-  });
+	const feed = new RSS({
+		title: `Amir R Muntasser's ${filter === 'all' ? 'Firehose' : filter === 'posts' ? 'Blog Posts' : 'Weaknotes'}`,
+		site_url: `${siteUrl}${filter === 'all' ? '' : filter === 'posts' ? '/blog' : '/weaknotes'}`,
+	});
 
   posts
-		.filter(post => {
-			if (filter === 'all') return true;
-			else if (filter === 'posts') return post.frontMatter.weeknotes === false;
-			else return post.frontMatter.weeknotes === true;
-		})
-		.forEach(post => {
-			feed.item({
-				title: post.frontMatter.title,
-				guid: post.url,
-				url: siteUrl + post.url,
-				date: post.frontMatter.date,
-			});
+	.filter(post => {
+		if (filter === 'all') return true;
+		else if (filter === 'posts') return post.frontMatter.weeknotes === false;
+		else return post.frontMatter.weeknotes === true;
+	})
+	.forEach(post => {
+		feed.item({
+			title: post.frontMatter.title,
+			guid: post.url,
+			url: siteUrl + post.url,
+			date: post.frontMatter.date,
 		});
+	});
 
-  return feed.xml({ indent: true })
+	return feed.xml({ indent: true })
 }
 
 async function init() {
@@ -33,7 +33,7 @@ async function init() {
 	const rssPathAll = 'public/rss-all.xml';
 	const rssAll = generateRSS(nodes);
 	fs.writeFileSync(rssPathAll, rssAll);
-  console.info(`Saved RSS feed to ${rssPathAll}`);
+	console.info(`Saved RSS feed to ${rssPathAll}`);
 
 	const rssPathPosts = 'public/rss-posts.xml';
 	const rssPosts = generateRSS(nodes, 'posts');
