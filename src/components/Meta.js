920b22ee017406d85e19545354288d898589877c
Amir R Muntasser <arkmuntasser@gmail.com>
2021-06-23 18:21:42 -0700

    added dark mode

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -6,6 +6,8 @@ export default function Meta({ title, children }) {
 			<title>ARKM - {title}</title>
 			<link rel="icon" href="/heart.svg"/>
 			<link rel="mask-icon" href="/heart.svg" color="#ff0055"/>
+			<meta name="theme-color" content="#fff" media="(prefers-color-scheme: light)"/>
+			<meta name="theme-color" content="#000" media="(prefers-color-scheme: dark)"/>
 			{children}
 		</Head>
 	)
