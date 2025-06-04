1968ddbd6a883053c26d4fc03b335448fe52aed3
Amir R Muntasser <arkmuntasser@gmail.com>
2025-06-03 21:28:39 -0700

    fixes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,9 @@
-import { Inter } from '@next/font/google';
+import { Inter } from "next/font/google";
 import Header from './Header';
 import Footer from './Footer';
 import styles from '../styles/Layout.module.css'
 
-const inter = Inter({ weight: ['400', '700', '900'] });
+const inter = Inter({ weight: ['400', '700', '900'], subsets: ['latin'] });
 const layoutClass = `${styles.layout} ${inter.className}`;
 
 export default function Layout({ children }) {
--- src/components/Meta.js
+++ src/components/Meta.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function Meta({ title, children }) {
 	return (
 		<Head>
-			<title>ARKM - {title}</title>
+			<title>{`arkm - ${title}`}</title>
 			<link rel="icon" href="/heart.svg"/>
 			<link rel="mask-icon" href="/heart.svg" color="#ff0055"/>
 			<meta name="theme-color" content="#fff" media="(prefers-color-scheme: light)"/>
